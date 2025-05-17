e8a7e5180740a40a64e04bea1b4b98333a3fbe3f
Darshan Kakkad <darshankakkad@gmail.com>
2025-05-17 11:25:20 +0100

    Update viztool.py

--- ifuviz/viztool.py
+++ ifuviz/viztool.py
@@ -8,26 +8,25 @@ def initiate_viztool(cube, model_cube, wl_array):
     cube[np.isnan(cube)] = 0
     collapse_img = np.sum(cube, axis=0)
 
-    fig, (ax0, ax1) = plt.subplots(2, 1, figsize=(8, 6), gridspec_kw={"height_ratios": [1, 2]})
-    img = ax0.imshow(collapse_img, origin='lower', cmap='hot',\
-                     extent=[-Nx/2,Nx/2,-Ny/2,Ny/2])
+    fig, (ax0, ax1) = plt.subplots(2, 1, figsize=(8, 9.2), gridspec_kw={"height_ratios": [2, 1]})
+    img = ax0.imshow(collapse_img, origin='lower', cmap='hot')
 
     spec_line, = ax1.plot([], [], label='Data', color='grey')
-    model_line, = ax1.plot([], [], label='Model', color='red')
+    #model_line, = ax1.plot([], [], label='Model', color='red')
 
     def onclick(event):
         if event.inaxes == ax0:
             i, j = int(event.xdata), int(event.ydata)
             spectrum = cube[:, j, i]
-            model_spec = model_cube[:, j, i]
+            #model_spec = model_cube[:, j, i]
             spec_line.set_data(wl_array, spectrum)
             model_line.set_data(wl_array, model_spec)
             ax1.set_margins(x=0)
             fig.canvas.draw_idle()
 
-    ax1.set_title("TBC: From header")
-    ax1.set_xlabel("Wavelength [arb unit]")
-    ax1.set_ylabel("Flux [arb unit]")
+    ax1.set_title("Click on a pixel to view the spectrum")
+    ax1.set_xlabel("Wavelength")
+    ax1.set_ylabel("Flux")
     ax1.legend()
 
     fig.canvas.mpl_connect('button_press_event', onclick)
