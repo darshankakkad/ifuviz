0dc67965abe38d90611e7d6785d1e66c46ace940
Darshan Kakkad <darshankakkad@gmail.com>
2025-05-17 13:51:58 +0100

    Add files via upload

--- ifuviz/__init__.py
+++ ifuviz/__init__.py
@@ -2,5 +2,5 @@ from ifuviz.viztool import initiate_viztool
 from ifuviz.loaddata import load_cube
 
 def main():
-    sci_cube, wl = load_cube()
+    sci_cube, wl = load_cube("/Users/dk24abv/work/research_projects/BASS/MUSE/LZIFU/data/NGC7469.fits")
     initiate_viztool(sci_cube, wl)
--- ifuviz/loaddata.py
+++ ifuviz/loaddata.py
@@ -1,10 +1,14 @@
 import astropy.io.fits as fits
 import numpy as np
 
-def load_cube():
-    cube_file = fits.open("/Users/dk24abv/work/research_projects/BASS/MUSE/LZIFU/data/NGC7469.fits")
+def load_cube(input_file):
+    # Load data
+    cube_file = fits.open(input_file)
     hdr = cube_file[0].header
     sci_cube = cube_file[0].data
     Nz, Ny, Nx = np.shape(sci_cube)
     wl = (np.linspace(1,Nz,Nz) - hdr["CRPIX3"])*hdr["CDELT3"] + hdr["CRVAL3"]
+
+    #model_file = fits.open("/Users/dk24abv/work/research_projects/BASS/MUSE/LZIFU/products/NGC7469_1_comp.fits")
+    #cont_cube = model_file[1].data
     return sci_cube, wl
